e07a0ca293143102fa75e5e11367c253b558bf45
Ruben Martins <rmartins@cs.utexas.edu>
2018-02-02 12:44:25 -0500

    feat: reachability analysis of petri-nets using constraint solving

---
+++ symonster/src/edu/cmu/reachability/Encoding.java
@@ -0,0 +1,28 @@
+package edu.cmu.reachability;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
+import uniol.apt.adt.pn.PetriNet;
+import uniol.apt.adt.pn.Place;
+import uniol.apt.adt.pn.Transition;
+import java.util.HashMap;
+import java.util.Set;
+
+public interface Encoding {
+	
+	// Maps the pair <transition in the petri-net, timestamp> to variable
+	HashMap<Pair<Transition, Integer>,Variable> transition2variable = new HashMap<>();
+	
+	// Maps the triple <place in the petri-net, timestamp, value> to variale
+	HashMap<Triple<Place, Integer, Integer>,Variable> place2variable = new HashMap<>();
+	
+	SATSolver solver = new SATSolver();
+	
+	public void setPetriNet(PetriNet pnet);
+	
+	public void setState(Set<Pair<Place, Integer>> state, int timestep);
+	
+	public void createConstraints();
+	
+	public void createVariables();
+}
---
+++ symonster/src/edu/cmu/reachability/Reachability.java
@@ -0,0 +1,13 @@
+package edu.cmu.reachability;
+
+import uniol.apt.adt.pn.PetriNet;
+
+public class Reachability {
+	
+	private PetriNet pnet;
+	
+	public Reachability(PetriNet pnet) {
+		this.pnet = pnet;
+	}
+
+}
