8ccbaca6931e6377320e26c1735d5f771d283ac2
anlunx <anlunx@andrew.cmu.edu>
2018-04-20 10:39:44 -0400

    added timeout function

--- symonster/run-all.py
+++ symonster/run-all.py
@@ -1,27 +1,54 @@
 import sys
 import os
 import fnmatch
+from contextlib import contextmanager
+import subprocess, threading
+
+class Command(object):
+    def __init__(self, cmd):
+        self.cmd = cmd
+        self.process = None
+
+    def run(self, timeout):
+        def target():
+            print 'Thread started'
+            self.process = subprocess.Popen(self.cmd, shell=True)
+            self.process.communicate()
+            print 'Thread finished'
+
+        thread = threading.Thread(target=target)
+        thread.start()
+
+        thread.join(timeout)
+        if thread.is_alive():
+            print 'Thread timeout'
+            self.process.terminate()
+            thread.join()
+        print self.process.returncode
 
 matches = []
 for root, dirnames, filenames in os.walk('benchmarks/'):
     for filename in fnmatch.filter(filenames, '*.json'):
         matches.append(os.path.join(root, filename))
 
-
-matches = ['benchmarks/geometry/10/benchmark10.json']
 with open('result.txt', 'w') as resultFile:
     for path in matches:
+        print (path)
+        resultFile.write(path + "\n")
         Dargs = ['"' + path + ' temp.txt -c"', 
                 '"' + path + ' temp.txt"',
-                '"' + path + ' temp.txt -e"',
-                '"' + path + ' temp.txt -cp"',
+                '"' + path + ' temp.txt -c -e"',
+                '"' + path + ' temp.txt -c -cp"',
                 '"' + path + ' temp.txt -e -cp"']
-
         for Darg in Dargs:
-            os.system('ant symonster -Dargs=' + Darg)
+            print ("settings:" + Darg)
+            command = Command('ant symonster -Dargs=' + Darg + ' &>/dev/null')
+            command.run(timeout=900)
             with open('temp.txt') as tempFile:
                 resultFile.write(tempFile.read())
                 resultFile.write("\n")
+        print ("\n")
+        resultFile.write("\n")
 
 
 
